afb97e9c27f5321e61e8a733b4e24d89995f9104
Roberto Rabacchi <user@MacBook-Pro.local>
2025-12-20 05:33:24 +0100

    fix: add null initializer to useRef in Scene.tsx

--- components/3d/Scene.tsx
+++ components/3d/Scene.tsx
@@ -7,7 +7,7 @@ import * as random from 'maath/random/dist/maath-random.cjs';
 import { motion } from 'framer-motion-3d';
 
 function Stars(props: any) {
-  const ref = useRef<any>();
+  const ref = useRef<any>(null);
   const [sphere] = useState(() => random.inSphere(new Float32Array(5000 * 3), { radius: 1.5 }));
 
   useFrame((state, delta) => {
